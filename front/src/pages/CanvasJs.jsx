27182e590ad92e4a01c97739adf17b2205c38b28
lasse <t8hula00@students.oamk.fi>
2021-11-15 17:23:21 +0200

    finally drawing a grid works

--- front/src/pages/CanvasJs.jsx
+++ front/src/pages/CanvasJs.jsx
@@ -1,42 +1,70 @@
-
 import * as React from 'react';
 import {useState, useEffect, useRef} from 'react'
 
 const Canvas = props => {
 
   var [ctx, setctx] = useState([])
-  let [scaleX, setXScale] = useState(0)
-  let [scaleY, setYScale] = useState(0)
-  let [rect, setRect] = useState()
+  var [size, setSize]= useState(50)
 
-  //set up canvas
+
+  //set up canvas, redraws canvas on screen size change
   useEffect(() => {
     let canvas = canvasRef.current
     canvas.setAttribute('width', window.innerWidth)
     canvas.setAttribute('height',window.innerHeight)
     const context = canvas.getContext('2d')
-    setctx(context)
-    //setXScale = context.width / rect.width
-    //setYScale = context.height / rect.height
+    
+    context.fillStyle = 'white'
+    context.fillRect(0, 0, context.canvas.width, context.canvas.height)
+    setctx(context)    
+    
+    let i = 0
+    while(i < canvas.width){
+      context.moveTo(i, 0)
+      context.lineTo(i, canvas.height)
+      i = i + 50
+    }
+    i = 0
+    while(i < canvas.height){
+      context.moveTo(0, i)
+      context.lineTo(canvas.width, i)
+      context.strokeStyle = 'black'
+      i = i + 50
+    }
+    context.strokeStyle = 'black'
+    context.lineWidth = 3
+    context.stroke()
+
+    /* for loop takes up too much memory for some reason, using 2 while loops works for some reason?
+    for (let i = 0; i <= canvas.width; i + 50){
+      context.moveTo(i, 0)
+      context.lineTo(i, canvas.height)
+      context.strokeStyle = 'black'
+      context.stroke()
+    }
+    for (let o = 0; o <= canvas.height; o + 50){
+      context.moveTo(0, o)
+      context.lineTo(canvas.width, o)
+      context.strokeStyle = 'black'
+      context.stroke()
+    }*/
+
     //'https://upload.wikimedia.org/wikipedia/commons/7/70/Graph_paper_scan_1600x1000_%286509259561%29.jpg'
-  }, [])
+  }, [window.innerWidth, window.innerHeight])
 
 
   //current x and previous x
   var [currX, setXn] = useState(0)
   var [prevX, setXp] = useState(0)
 
-
   //current y and previous y
   var [currY, setYn] = useState(0)
   var [prevY, setYp] = useState(0)
   
-
   //is drawing allowed?
   var [flag, setFlag] = useState(false)
 
   const canvasRef = useRef(null)
-
 
   var draw = () => {
     ctx.beginPath();
@@ -47,9 +75,6 @@ const Canvas = props => {
     ctx.stroke();
     ctx.closePath();
   }
-
-
-
 
   var begin = (x,y,name) =>{
   
@@ -78,9 +103,6 @@ const Canvas = props => {
     }
     
   }
-  
-
-
 
 
   return <canvas 
