4b4fb460d23fa4d026d0a5e8fe9184139e563cc9
lasse <t8hula00@students.oamk.fi>
2021-11-30 16:24:04 +0200

    multiple object can be rendered, transformcontrols go to clicked object

--- front/src/pages/canvas/CanvasThree.jsx
+++ front/src/pages/canvas/CanvasThree.jsx
@@ -1,47 +1,57 @@
-import React, { useRef, useState, useEffect, Suspense } from 'react'
+import { useRef, useState, useEffect, Suspense } from 'react'
 import ReactDOM from 'react-dom'
 import { Canvas, useFrame, extend } from '@react-three/fiber'
-import { OrbitControls} from "@react-three/drei"
+import { OrbitControls, PerspectiveCamera, TransformControls} from "@react-three/drei"
 
+export default function ThisCanvas (props){
+  let [controlTarget, setTarget] = useState()
+  var [shapeArray, setShapeArray] = useState([])
+  let [size, divs] = [100,100]
+    
+  useEffect(()=>{
+    setShapeArray([<Box position={[0, 0, 0]} />])
+  }, [])
 
+  let addShape =()=>{
+    let [x, z] = [(Math.random() * size/2) * (Math.round(Math.random()) ? 1 : -1), (Math.random() * size/2) * (Math.round(Math.random()) ? 1 : -1)]
+    setShapeArray([...shapeArray, <Box position={[x, 0,z]}/>])
+  }
 
-function Box(props) {
-  // This reference will give us direct access to the mesh
-  const mesh = useRef()
-  // Set up state for the hovered and active state
-  const [hovered, setHover] = useState(false)
-  const [active, setActive] = useState(false)
-  // Rotate mesh every frame, this is outside of React without overhead
-  useFrame(() => (mesh.current.rotation.x += 0.01))
-
-  return (
-    <mesh
-      {...props}
-      ref={mesh}
-      scale={active ? 1.5 : 1}
-      onClick={(event) => setActive(!active)}
-      onPointerOver={(event) => setHover(true)}
-      onPointerOut={(event) => setHover(false)}>
-      <boxGeometry args={[1, 1, 1]} />
-      <meshStandardMaterial color={hovered ? 'hotpink' : 'orange'} />
-    </mesh>
-  )
-}
-
-
-export default function thisCanvas (props){
+  function Box(props) {
+    // This reference will give us direct access to the mesh
+    const mesh = useRef()
+    // Set up state for the hovered and active state
+    const [hovered, setHover] = useState(false)
+    // Rotate mesh every frame, this is outside of React without overhead
+    useFrame(() => (mesh.current.rotation.x += Math.random()*(0.1 - 0)  +0))
+    return (
+      <mesh
+        {...props}
+        ref={mesh}
+        onClick={(event) => {setTarget(mesh.current)}}
+        onPointerOver={(event) => setHover(true)}
+        onPointerOut={(event) => setHover(false)}>
+          <boxGeometry args={[1, 1, 1]} />
+          <meshStandardMaterial color={hovered ? 'hotpink' : 'orange'} />
+      </mesh>
+    )
+  }
+  
+  var checkPos=(target)=>{
+    console.log(target.worldPosition.x, target.worldPosition.y, target.worldPosition.z)
+  }
 
     return <>
-        <Canvas id="models">
-          <gridHelper/>
-          <spotLight position={[10, 10, 10]} angle={0.15} penumbra={1} />
-            <pointLight position={[-10, -10, -10]} />
-          
-          <Box position={[-1.2, 0, 0]} />
-          <OrbitControls camera={props.camera}>
-          </OrbitControls>
-          
-
+        <button onClick={()=>addShape()}>add box</button>
+        <Canvas id="models" onClick={()=>console.log("perjelk")}>
+            <pointLight position={[10, 10, 10]} />        
+          <gridHelper args={[size,divs]}/>
+          <TransformControls mode="translate" object={controlTarget}>
+          </TransformControls>
+          <OrbitControls makeDefault/>
+          {shapeArray}
         </Canvas>
     </>
 }
+
+
