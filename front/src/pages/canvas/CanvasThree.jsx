cbce164ae5c707f2daa3523d2c30595194b126cc
lasse <t8hula00@students.oamk.fi>
2021-12-04 14:05:14 +0200

    going to joensuu, need progress on laptop

--- front/src/pages/canvas/CanvasThree.jsx
+++ front/src/pages/canvas/CanvasThree.jsx
@@ -1,60 +1,74 @@
 import { useRef, useState, useEffect, Suspense } from 'react'
 import ReactDOM from 'react-dom'
-import { Canvas, useFrame, extend } from '@react-three/fiber'
+import { Canvas, useThree } from '@react-three/fiber'
 import { OrbitControls, PerspectiveCamera, TransformControls, Line } from "@react-three/drei"
+import {IonToggle} from '@ionic/react'
+
 
 export default function ThisCanvas(props) {
   let [controlTarget, setTarget] = useState()
-  var [shapeArray, setShapeArray] = useState([])
+  let [shapeArray, setShapeArray] = useState([])
   let [size, divs] = [100, 100]
   const [checked, setChecked] = useState(false);
 
-  useEffect(() => {
-    setShapeArray([<Line
-      points={[[0, 0, 0], [-1.2, 0, 0]]}       // Array of points
-      color="red"                   // Default
-      lineWidth={1}                   // In pixels (default)
-      dashed={true}                  // Default
+  let [oldTarget, setOldTarget] = useState()
 
-    />])
-  }, [])
-
-  let addShape = () => {
-    let [x, z] = [(Math.random() * size / 2) * (Math.round(Math.random()) ? 1 : -1), (Math.random() * size / 2) * (Math.round(Math.random()) ? 1 : -1)]
-    setShapeArray([...shapeArray, <Box position={[x, 0, z]} />])
+  
+  //position update functiona
+  let getPos = (pos, id) => {
+  
   }
 
-  function Box(props) {
+  //console.log(shapeArray.find(i=> i.key==null))
+
+  let addShape = (newSize) => {
+    let [x, z] = [(Math.random() * size / 2) * (Math.round(Math.random()) ? 1 : -1), (Math.random() * size / 2) * (Math.round(Math.random()) ? 1 : -1)]
+    //setShapeArray([...shapeArray, <Cylinder position={[x, 0, z]} />])
+    setShapeArray([...shapeArray, <Cylinder position={[0, 0, 0]} newSize={newSize} /*getPos={(pos, id)=>getPos(pos,id)}*//>])
+  }
+  console.log(shapeArray.length)
+  function Cylinder(props) {
     // This reference will give us direct access to the mesh
     const mesh = useRef()
-    // Set up state for the hovered and active state
     const [hovered, setHover] = useState(false)
-    // Rotate mesh every frame, this is outside of React without overhead
-    useFrame(() => (mesh.current.rotation.x += Math.random() * (0.1 - 0) + 0))
+    let newSize
+    switch(props.newSize) {
+      case "small": {newSize = [0.25, 0.25, 0.5,8,1]} break;
+      case "medium": {newSize = [0.5, 0.5, 0.5,8,1]} break;
+      case "large": {newSize = [1, 1, 0.5,8,1]} break;
+      case "huge": {newSize = [1.5, 1.5, 0.5,8,1]} break;
+      case "gargantuan": {newSize = [2, 2, 0.5,8,1]} break;
+    }
+
+    //update position of this
+    useEffect(() => {
+      if(mesh.current.position)
+      {
+        let id = mesh.current.uuid
+        props.getPos(mesh.current.position, id)
+      }
+    })
     return (
       <mesh
         {...props}
         ref={mesh}
-        onClick={(event) => { setTarget(mesh.current) }}
+        onClick={(event) => { setTarget(mesh.current)}}
         onPointerOver={(event) => setHover(true)}
         onPointerOut={(event) => setHover(false)}>
-        <boxGeometry args={[1, 1, 1]} />
+        <cylinderGeometry args={newSize} />
         <meshStandardMaterial color={hovered ? 'hotpink' : 'orange'} />
       </mesh>
     )
   }
 
-  var checkPos = (target) => {
-    console.log(target.worldPosition.x, target.worldPosition.y, target.worldPosition.z)
-  }
-
   return <>
-    <button onClick={() => addShape()}>add box</button>
+    <button onClick={() => addShape("gargantuan")}>add box</button>
+    <button >get Json data</button>
     <IonToggle checked={checked}/>
-    <Canvas id="models" onClick={() => console.log("perjelk")}>
+    <Canvas id="models">
       <pointLight position={[10, 10, 10]} />
-      <gridHelper args={[size, divs]} />
-      <TransformControls mode="translate" object={controlTarget}>
+      <gridHelper args={[size, divs]}/>
+      <TransformControls mode="translate" object={controlTarget} size={1} translationSnap={1} onObjectChange={}>
       </TransformControls>
       <OrbitControls makeDefault />
       {shapeArray}
