168cdacab55a72313838552497388f676a77902e
Lasse Huovinen <t8hula00@students.oamk.fi>
2021-12-08 11:18:37 +0200

    pos of mesh can be found by matching shape.key to mesh.id, shapearray now works properly instead of stacking arrays into arrays

--- front/src/pages/canvas/CanvasThree.jsx
+++ front/src/pages/canvas/CanvasThree.jsx
@@ -8,23 +8,32 @@ import {IonToggle} from '@ionic/react'
 export default function ThisCanvas(props) {
   let [controlTarget, setTarget] = useState()
   let [shapeArray, setShapeArray] = useState([])
+  let [tempVars, setTempVars] = useState({})
+  let [meshData, setMeshData] = useState([])
   let [size, divs] = [100, 100]
   const [checked, setChecked] = useState(false);
-  
+
 
   useEffect(() => {
     setShapeArray([])
+    setMeshData([])
   }, [])
 
-  let addShape = (newSize) => {
-    let [x, z] = [(Math.random() * size / 2) * (Math.round(Math.random()) ? 1 : -1), (Math.random() * size / 2) * (Math.round(Math.random()) ? 1 : -1)]
-    //setShapeArray([...shapeArray, <Cylinder position={[x, 0, z]} />])
-    setShapeArray([...shapeArray, <Cylinder position={[0, 0, 0]} newSize={newSize} /*getPos={(pos, id)=>getPos(pos,id)}*//>])
+  let getId= (id) =>{
+    return id
   }
 
+  let addShape = (newSize) => {
+    let [x, z] = [Math.floor(Math.random() * size / 2) * (Math.round(Math.random()) ? 1 : -1), Math.floor(Math.random() * size / 2) * (Math.round(Math.random()) ? 1 : -1)]
+    //setShapeArray([...shapeArray, <Cylinder position={[x, 0, z]} />])
+    let tempArray = shapeArray.flat()
+    let newShape =  <Cylinder position={[x, 0, z]} newSize={newSize} /*getPos={(pos, id)=>getPos(pos,id)}*/keyid={tempArray.length} key={tempArray.length}/>
+    tempArray.push(newShape)
+    setShapeArray([])
+    setShapeArray([tempArray.flat()])
+  }
 
   let line = checked ? <Line points={[[0, 0, 0], [-1.2, 0, 0]]} color="red" lineWidth={1} dashed={true} /> :null
-  console.log(shapeArray.length)
   function Cylinder(props) {
     // This reference will give us direct access to the mesh
     const mesh = useRef()
@@ -38,27 +47,34 @@ export default function ThisCanvas(props) {
       case "gargantuan": {newSize = [2, 2, 0.5,8,1]} break;
     }
 
-    //update position of this
+    useEffect(()=>{
+      let meshObj = {"id":props.keyid, "mesh" : mesh.current}
+      let tempdata = meshData
+      tempdata.push(meshObj)
+      setMeshData(tempdata)
+    },[])
+
     useEffect(() => {
       if(mesh.current.position)
       {
-        let id = mesh.current.uuid
-        props.getPos(mesh.current.position, id)
+        //props.getPos(mesh.current.position, id)
       }
     })
+
+
     return (
       <mesh
         {...props}
         ref={mesh}
         onClick={(event) => { setTarget(mesh.current)}}
-        onPointerOver={(event) => setHover(true)}
+        onPointerOver={(event) => {setHover(true); meshData.map(i=>console.log(i.mesh.position))}}
         onPointerOut={(event) => setHover(false)}>
         <cylinderGeometry args={newSize} />
         <meshStandardMaterial color={hovered ? 'hotpink' : 'orange'} />
       </mesh>
     )
   }
-
+  meshData.map(i=> console.log(i.mesh.position))
   return <>
     <button onClick={() => addShape("gargantuan")}>add box</button>
     <button >get Json data</button>
@@ -66,7 +82,7 @@ export default function ThisCanvas(props) {
     <Canvas id="models">
       <pointLight position={[10, 10, 10]} />
       <gridHelper args={[size, divs]}/>
-      <TransformControls mode="translate" object={controlTarget} size={1} translationSnap={1} onObjectChange={}>
+      <TransformControls mode="translate" object={controlTarget} size={1} translationSnap={1}>
       </TransformControls>
       <OrbitControls makeDefault />
       {shapeArray}
@@ -74,5 +90,3 @@ export default function ThisCanvas(props) {
     </Canvas>
   </>
 }
-
-
