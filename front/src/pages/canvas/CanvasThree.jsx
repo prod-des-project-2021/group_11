db09f6381ce3436ac74ec7bd3338e405efb2e9b0
Nabeel <nabeelbaig962@gmail.com>
2021-12-08 14:11:39 +0200

    making lines by clicking (work-in-progress)

--- front/src/pages/canvas/CanvasThree.jsx
+++ front/src/pages/canvas/CanvasThree.jsx
@@ -1,7 +1,7 @@
 import { useRef, useState, useEffect, Suspense } from 'react'
 import ReactDOM from 'react-dom'
 import { Canvas, useFrame, extend } from '@react-three/fiber'
-import { OrbitControls, PerspectiveCamera, TransformControls, Line } from "@react-three/drei"
+import { OrbitControls, PerspectiveCamera, TransformControls, Line, } from "@react-three/drei"
 import { IonToggle } from '@ionic/react';
 
 export default function ThisCanvas(props) {
@@ -9,7 +9,7 @@ export default function ThisCanvas(props) {
   var [shapeArray, setShapeArray] = useState([])
   let [size, divs] = [100, 100]
   const [checked, setChecked] = useState(false);
-  
+
 
   useEffect(() => {
     setShapeArray([])
@@ -20,10 +20,28 @@ export default function ThisCanvas(props) {
     setShapeArray([...shapeArray, <Box position={[x, 0, z]} />])
   }
 
-  let line = checked ? <Line points={[[0, 0, 0], [-1.2, 0, 0]]} color="red" lineWidth={1} dashed={true} /> :null
+  //current x and previous x
+  var [currX, setXn] = useState(0)
+  var [prevX, setXp] = useState(0)
 
-  
+  //current y and previous y
+  var [currY, setYn] = useState(0)
+  var [prevY, setYp] = useState(0)
 
+  //current z and previous z
+  var [currZ, setZn] = useState(0)
+  var [prevZ, setZp] = useState(0)
+
+  let line = checked ? <Line points={[[prevX, prevY, prevZ], [currX, currY, currZ]]} color="red" lineWidth={1} dashed={true} /* {...lineProps}  All THREE.Line2 props are valid {...materialProps} /* All THREE.LineMaterial props are valid */ /> : null
+
+
+  var begin = (x, y, z, name) => {
+    switch(name) {
+      case "down": {
+        
+      }
+    }
+  }
 
 
   function Box(props) {
@@ -61,6 +79,8 @@ export default function ThisCanvas(props) {
       <OrbitControls makeDefault />
       {shapeArray}
       {line}
+
+      onMouseDown={(e) => begin(e.pageX, e.pageY, e.pageZ, "down")}
     </Canvas>
   </>
 }
