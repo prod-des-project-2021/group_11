e538ba0b187bfbd3308dba5ec088ac33bd9b4d05
Lasse Huovinen <t8hula00@students.oamk.fi>
2021-12-10 07:52:30 +0200

    back-dev main.rs now matches three-js main.rs

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -1,12 +1,21 @@
 #![feature(proc_macro_hygiene, decl_macro)]
-
+use std::time::SystemTime;
 use rocket::*;
 
 #[get("/")]
 fn index() -> &'static str {
     "Hello, world!"
+
+}
+#[get("/lol")]
+fn handler(){
+    let time = SystemTime::now();
+    println!("{:?}", time)
 }
 
+
 fn main() {
-    rocket::ignite().mount("/", routes![index]).launch();
+    rocket::ignite().mount("/", routes![index])
+    .mount("/", routes![handler])
+    .launch();
 }
